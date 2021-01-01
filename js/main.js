c3bdfc8d02c7d114089b184e2c2febb94bd1f797
Tammy Truong <tammytruong@berkeley.edu>
2020-12-31 18:34:05 -0800

    Mobile view

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// Active section
+const showActive = 
+
+// Show menu
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -9,3 +13,13 @@ const showMenu = (toggleId, navId) =>{
     }
 }
 showMenu('navbar-toggle','navbar-menu')
+
+// Hide menu after click
+
+const navLink = document.querySelectorAll('.navbar__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('navbar-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
